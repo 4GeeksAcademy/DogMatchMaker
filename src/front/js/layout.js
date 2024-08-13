738df1d8ceb42aa63fa470f26ba45d2f8d3843e2
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-08-13 15:47:38 -0400

    resolve the conflicts

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,14 +1,8 @@
-
-<<<<<<< HEAD
 import React, { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-=======
-import React from "react";
-import { BrowserRouter, Route, useLocation , Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop.jsx";
->>>>>>> landing_page_faith_is_work
 import { BackendURL } from "./component/backendURL";
+import injectContext, { Context } from "./store/appContext";
 
 import { Home } from "./pages/home.jsx";
 import { Login } from "./pages/login";
@@ -16,9 +10,9 @@ import { Signup } from "./pages/signup";
 import { Private } from "./pages/private";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-<<<<<<< HEAD
-import injectContext, { Context } from "./store/appContext";
-
+import { Contact } from "./pages/Contact.jsx"
+import { Pricing } from "./pages/Pricing.jsx"
+import { About } from "./pages/About.jsx"
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import Messages from "./pages/messages.jsx";
@@ -26,15 +20,6 @@ import Sidebar from "./component/sidebar.jsx";
 import Profile from "./pages/profile.jsx";
 import Notifications from "./component/notifications.jsx";
 import Settings from "./pages/settings.jsx";
-=======
-import { Pricing } from "./pages/Pricing.jsx"
-import { About } from "./pages/About.jsx"
-import injectContext from "./store/appContext";
-import { Contact } from "./pages/Contact.jsx"
-
-import { Navbar } from "./component/navbar.jsx";
-import { Footer } from "./component/footer.jsx";
->>>>>>> landing_page_faith_is_work
 
 //create your first component
 const Layout = () => {
@@ -46,13 +31,10 @@ const Layout = () => {
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
-<<<<<<< HEAD
-        <div className="container-fluid">
-=======
+
         <div className="container-fluid px-0">
->>>>>>> landing_page_faith_is_work
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
+                <ScrollToTop>   
                     <Navbar />
                     {(store.token && store.token != null && store.token != undefined ) && <Sidebar/>}
                     <Routes>

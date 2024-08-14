34e58369062c18a95fb7546ced81edb320880b6c
Jeff <156033827+jeff-c44@users.noreply.github.com>
2024-08-14 16:19:15 +0000

    resolved

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,16 +1,32 @@
-from flask import Blueprint, request, jsonify
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+from flask import Flask, request, jsonify, Blueprint
 from api.models import db, UserAccount
-from api.utils import APIException
-from flask_cors import cross_origin
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from api.utils import generate_sitemap, APIException
+from flask_cors import CORS, cross_origin
+import json
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, set_access_cookies, unset_jwt_cookies
+import requests
 from werkzeug.utils import secure_filename
 import os
 
 api = Blueprint('api', __name__)
 
+# Allow CORS requests to this API
+CORS(api, supports_credentials=True)
+
+# Define the path where profile pictures will be stored
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+    return jsonify(response_body), 200
 
 @api.route('/user', methods=['POST'])
 @cross_origin()
@@ -35,7 +51,7 @@ def create_user():
     
     userAccount = UserAccount(
         email=email,
-        password=password,
+        password=password,  # Note: Password hashing should be handled in the model or service layer
         dog_name=dog_name,
         owner_name=owner_name,
         nick_name=nick_name,
@@ -64,13 +80,17 @@ def create_token():
     email = data.get("email")
     password = data.get("password")
 
-    userAccount = UserAccount.query.filter_by(email=email, password=password).first()
+    userAccount = UserAccount.query.filter_by(email=email).first()
 
-    if userAccount is None:
+    if userAccount is None or not userAccount.check_password(password):  # Ensure you check hashed password
         return jsonify({"msg": "Bad email or password"}), 401
     
     access_token = create_access_token(identity=userAccount.email)
-    return jsonify({ "access_token": access_token })
+    response = jsonify({"access_token": access_token})
+    
+    set_access_cookies(response, access_token)
+    
+    return response
 
 @api.route('/private', methods=['GET'])
 @jwt_required()
@@ -80,7 +100,7 @@ def private_hello():
 
     response_body = {
         "section": "Private",
-        "message": "Hello " + str(userAccount)
+        "message": "Logged in as "+str(userAccount)
     }
 
     return jsonify(response_body), 200
@@ -93,3 +113,40 @@ def get_users():
         "users": [user.serialize() for user in users]
     }
     return jsonify(response_body), 200
+
+@api.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"message": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+@api.route("/userChat", methods=['POST'])
+def create_user_chat():
+    name = request.json.get("name", None)
+    
+    resp = requests.post("https://api.chatengine.io/users",
+                        headers={"PRIVATE-KEY": "bf275b72-da68-4bd2-8f95-a00436f7aee7"},
+                        data={
+                            "username": name,
+                            "first_name": name,
+                            "last_name": ' ',
+                            "secret": "123456"
+                        })
+     
+    return jsonify(resp.json())
+
+@api.route("/privateChat", methods=['PUT'])
+def create_private_chat():
+    name = request.json.get("name", None)
+    guest = request.json.get("guest", None)
+    
+    resp = requests.put("https://api.chatengine.io/chats",
+                        headers={"Project-ID": "bf275b72-da68-4bd2-8f95-a00436f7aee7",
+                                 "User-Name" : name,
+                                 "User-Secret" : "123456"},
+                        data={
+                                "usernames": [guest],
+                                "is_direct_chat": True
+                            })
+        
+    return jsonify(resp.json())
